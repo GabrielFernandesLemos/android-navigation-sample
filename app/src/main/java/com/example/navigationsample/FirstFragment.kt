6aad869292580d74a6e455f8024f071d83fea25f
Gabriel Fernandes Lemos <gabriel_lemos03@hotmail.com>
2022-06-26 12:12:05 -0300

    Create fragments and navigation file

---
+++ app/src/main/java/com/example/navigationsample/FirstFragment.kt
@@ -0,0 +1,13 @@
+package com.example.navigationsample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class FirstFragment : Fragment(R.layout.fragment_first_navigation) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
