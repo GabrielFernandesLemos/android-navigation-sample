b15ce0f17a8b76b0691f0b6336dadbdf6aa0916e
Gabriel Fernandes Lemos <gabriel_lemos03@hotmail.com>
2022-06-26 12:38:02 -0300

    update fragments

--- app/src/main/java/com/example/navigationsample/SecondFragment.kt
+++ app/src/main/java/com/example/navigationsample/SecondFragment.kt
@@ -2,12 +2,14 @@ package com.example.navigationsample
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
-class FirstFragment : Fragment(R.layout.fragment_first) {
+class SecondFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
-
 }
